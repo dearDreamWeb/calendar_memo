ac45e5c539b6df14362e7cd74a46fbf8967d4e19
wxb <m18837120047@163.com>
2020-08-25 22:31:32 +0800

    v0.1 项目初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+// import { Button } from 'antd-mobile';
+import { Map } from 'react-amap';
+
 
 function App() {
+   //eslint-disable-next-line
+  const citySearch = new AMap.CitySearch();
+  useEffect(() => {
+    init()
+
+  }, [])
+
+  function init() {
+    citySearch.getLocalCity((status, result) => {
+      if (status === 'complete' && result.info === 'OK') {
+        // 查询成功，result即为当前所在城市信息
+        console.log('当前所在城市：', result)
+        if (result && result.city && result.bounds) {
+          // 当前城市名称
+          // const cityinfo = result.city;
+
+        
+        }
+      }
+    })
+    // citySearch.getLocalCity(function (status, result) {
+    //   if (status === 'complete' && result.info === 'OK') {
+    //     // 查询成功，result即为当前所在城市信息
+    //     console.log(result)
+    //   }
+    // })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '100%', height: '400px', position: 'relative' }}>
+        <Map amapkey={"84b770bc352b74056e4a1a21f3c8d109"}>
+
+        </Map>
+      </div>
     </div>
   );
 }
