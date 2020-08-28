1b75886b1f932ac5d9862544632213f54edec36e
wxb <m18837120047@163.com>
2020-08-28 10:58:27 +0800

    v0.4 农历已完成

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './index.scss';
-import { DatePicker, List } from 'antd-mobile';
+import { DatePicker } from 'antd-mobile';
+import GetLunarDay from "../../utils/lunarCalendar";
 
 const Calender = () => {
     const [itemsArr, setItemsArr] = useState(new Array(35));
@@ -14,17 +15,34 @@ const Calender = () => {
 
     let monthDays = new Date(year, month, 0).getDate();         // 本月有多少天
     let lastMonthDays = new Date(year, month - 1, 0).getDate();         // 上个月有多少天
-    // let nextMonthDays = new Date(year, month + 2, 0).getDate();     // 下个月有多少天
     let weekStart = new Date(year, month - 1, 1).getDay();          // 本月第一天是周几
 
+    useEffect(() => {
+        changeItemH();
+    }, []);
 
     useEffect(() => {
-        fillDate()
+        fillDate();
     }, [year, month, day]);
 
     useEffect(() => {
-        setSelectDay(day)
+        setSelectDay(day);
     }, [day])
+
+    useEffect(() => {
+        showLunarDay();
+    }, [monthStartIndex])
+
+    // 改变元素的高度和宽度一致
+    const changeItemH = () => {
+        setTimeout(() => {
+            const items = Array.from(document.getElementsByClassName("item"));
+            const itemW = window.getComputedStyle(items[0]).width;
+            items.forEach(el => {
+                el.style.height = itemW;
+            })
+        }, 500)
+    }
 
     // 把日期填充进数组
     const fillDate = () => {
@@ -32,32 +50,40 @@ const Calender = () => {
         itemsArr.unshift(...["日", "一", "二", "三", "四", "五", "六"]);
         // 这个月天数遍历
         for (let i = 1; i <= monthDays; i++) {
-            itemsArr[7 + weekStart + i - 1] = i
+            itemsArr[7 + weekStart + i - 1] = { solar: i }
         }
         // 上个月天数遍历
         let j = 0;
         for (let i = weekStart - 1; i >= 0; i--) {
-            itemsArr[7 + i] = lastMonthDays - j;
+            itemsArr[7 + i] = { solar: lastMonthDays - j };
             j++;
         }
+        // 计算出本月第一天是在itemsArr的下标值
+        let nowMonthStartIndex = 0;
+        itemsArr.forEach((item, i) => {
+            if (i > 6 && item.solar === 1) {
+                nowMonthStartIndex = i;
+            }
+        })
 
         // 填充下个月
         if (itemsArr.length % 7 !== 0 || itemsArr.includes(undefined)) {
             let k = 1;
             // 下个月开始的位置
-            let nextMonthStartIndex = itemsArr.indexOf(1) + monthDays;
+
+            let nextMonthStartIndex = nowMonthStartIndex + monthDays;
             // 下个月的出现的天数
             let nextMonthLength = itemsArr.length % 7 !== 0
                 ? 7 - itemsArr.length % 7
                 : itemsArr.length - nextMonthStartIndex;
             for (let i = 0; i < nextMonthLength; i++) {
-                itemsArr[i + nextMonthStartIndex] = k;
+                itemsArr[i + nextMonthStartIndex] = { solar: k };
                 k++;
             }
         }
 
-        setItemsArr(JSON.parse(JSON.stringify(itemsArr)));
-        setMonthStartIndex(itemsArr.indexOf(1));
+        setItemsArr([...itemsArr]);
+        setMonthStartIndex(nowMonthStartIndex);
     }
 
     // 判断是否是今天
@@ -83,7 +109,37 @@ const Calender = () => {
         setDay(date.getDate());
         setWeek(date.getDay());
         setItemsArr(new Array(35));
-        console.log(date.getDate())
+    }
+
+    // 判断是几月份
+    const diffMonth = (index) => {
+        console.log(monthStartIndex)
+        if (index >= monthStartIndex && index < monthStartIndex + monthDays) {
+            return month;
+        } else if (index > 6 && monthStartIndex > index) {
+            return month - 1;
+        } else if (index > monthStartIndex + monthDays) {
+            return month + 1;
+        }
+    }
+
+    // 把农历传入到itemsArr数组中
+    const showLunarDay = () => {
+        itemsArr.forEach((item, index) => {
+            if (index > 6) {
+                let readyMonth = diffMonth(index);
+                // console.log(readyMonth)
+                item.lunar = GetLunarDay(year, readyMonth, item.solar)
+            }
+        })
+        setItemsArr([...itemsArr]);
+    }
+
+    // 只要最后两个农历初几
+    const sliceLunar = (item) => {
+        let i = item.indexOf("月");
+        item = item.slice(i+1);
+        return item;
     }
 
     return (
@@ -118,13 +174,16 @@ const Calender = () => {
                                 } ${
                                 // 区分是不是已选中，再判断是不是今天
                                 selectDay === index - monthStartIndex + 1 && index >= monthStartIndex && index < monthStartIndex + monthDays
-                                    ? isToday(item)
+                                    ? isToday(item.solar)
                                         ? "today"
                                         : "selectDay"
                                     : ""
                                 }`
                             }
-                            key={index}>{item}
+                            key={index}
+                        >
+                            {index > 6 ? item.solar : item}
+                            <p className="lunar">{index > 6 ? sliceLunar(item.lunar) : ""}</p>
 
                         </div>)
                     )
