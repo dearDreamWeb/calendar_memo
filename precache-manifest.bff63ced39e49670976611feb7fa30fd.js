b41a838ce7b69f7074eb85d9a2f2e5a6f4987547
wxb <m18837120047@163.com>
2020-09-07 10:08:44 +0800

    v1.0

---
+++ precache-manifest.bff63ced39e49670976611feb7fa30fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b6cd8ff99a8b32c6c7a339b9d45591",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b288ff7ef50da279c11",
+    "url": "./static/css/2.d5982c66.chunk.css"
+  },
+  {
+    "revision": "c3384910699a55572328",
+    "url": "./static/css/main.e29635f6.chunk.css"
+  },
+  {
+    "revision": "2b288ff7ef50da279c11",
+    "url": "./static/js/2.d4b4387a.chunk.js"
+  },
+  {
+    "revision": "5ee320707bcfc074b39ef9b54cd49ba4",
+    "url": "./static/js/2.d4b4387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3384910699a55572328",
+    "url": "./static/js/main.2fd6765d.chunk.js"
+  },
+  {
+    "revision": "2da4bb55b2b67b97ed71",
+    "url": "./static/js/runtime-main.498473a5.js"
+  },
+  {
+    "revision": "f8456837c01e9fcabeec6e794888ea6b",
+    "url": "./static/media/orange.f8456837.png"
+  }
+]);
