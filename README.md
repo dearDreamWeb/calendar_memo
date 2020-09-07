9203b8387412da410b98eac61ff9e11feba4b687
wxb <m18837120047@163.com>
2020-09-07 10:07:15 +0800

    v3.0 markdown完成，判断是否为手机

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# 项目预览
+项目打包部署到了github和码云上。    
+
+
+<a href="https://flyingwxb.gitee.io/calendar_memo">码云版预览地址</a>（国内访问会很快）  
+<a href="https://deardreamweb.github.io/calendar_memo/">github版预览地址</a>（国内访问可能会有点慢，请耐心等待） 
 
 # 橙子日历
 基于React Hooks开发的移动端的日历工具的一款webApp，名为橙子日历。  
@@ -22,3 +28,18 @@ npm install
 npm start
 ```
 # 项目效果
+
+## 首页界面
+![首页界面](https://s1.ax1x.com/2020/09/07/wnPiuR.jpg)
+
+## 选择日期
+![选择日期](https://s1.ax1x.com/2020/09/07/wnPCv9.jpg)
+
+## 添加备忘录
+![添加备忘录](https://s1.ax1x.com/2020/09/07/wnPFD1.jpg)
+
+## 备忘录界面
+![备忘录界面](https://s1.ax1x.com/2020/09/07/wnPp34.jpg)
+
+## 删除备忘录
+![删除备忘录](https://s1.ax1x.com/2020/09/07/wnP9gJ.jpg)
--- src/views/app/index.js
+++ src/views/app/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer,useEffect } from "react";
+import React, { useReducer, useEffect, useState } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import './index.scss';
 import { WingBlank } from 'antd-mobile';
@@ -13,6 +13,18 @@ function App() {
   //  获取是state和dispatch 
   const [state, dispatch] = useReducer(reducer, initData);
 
+  const [isPhone, setIsPhone] = useState(false);
+
+  useEffect(() => {
+    phoneOrPc();
+  }, [])
+
+  const phoneOrPc = () => {
+    let result = navigator.userAgent.match(
+      /(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i
+    );
+    setIsPhone(result);
+  }
 
   return (
     // 获取到useContext中存的值
@@ -21,6 +33,28 @@ function App() {
       dispatch // 把 dispatch 也作为 context 的一部分共享下去，从而在嵌套组件中调用以实现更新顶层的 state
     }}>
       <div className="App">
+        {/* 判断是否是手机 */}
+        {isPhone
+          ? ""
+          : (<div className="tip-wrap">
+            <div className="tip">
+              <div
+                className="close"
+                onClick={() => setIsPhone(true)}
+              >X</div>
+              <h1 className="title">
+                请手机扫描二维码或者使用浏览器手机模式浏览
+                </h1>
+              <div className="img_wrap">
+                <img
+                  src="https://s1.ax1x.com/2020/09/07/wnPSCF.png"
+                  alt="二维码"
+                />
+              </div>
+            </div>
+          </div>)
+        }
+
         <WingBlank>
           <header className="header">
             <h1 className="title">
@@ -29,21 +63,22 @@ function App() {
           日历
           </h1>
           </header>
-            <Switch>
+          <Switch>
 
-              <Route exact path="/home" component={Calender} />
+            <Route exact path="/home" component={Calender} />
 
-              <Route exact path="/memo" component={Memo} />
+            <Route exact path="/memo" component={Memo} />
 
-              <Route exact path="/addmemo" component={AddMemo} />
+            <Route exact path="/addmemo" component={AddMemo} />
 
-              <Redirect from='/' to='/home'></Redirect>
-            </Switch>
+            <Redirect from='/' to='/home'></Redirect>
+          </Switch>
 
           {/* 底部组件 */}
           <DiyFooter />
 
         </WingBlank>
+
       </div>
     </ContextData.Provider>
 
